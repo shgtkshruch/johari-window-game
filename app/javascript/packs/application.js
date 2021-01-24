e0e0a1dde41dcdb8f93ca97530c4183340716916
shgtkshruch <shgtk.shruch@gmail.com>
2021-01-24 22:40:01 +0900

    `./bin/rails hotwire:install`

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,12 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "controllers"
